6e62fef997b08f00560d8d50f8e7728735679e0a
Aakankshap16 <aakankshap971@gmail.com>
2023-03-18 01:06:56 +0530

    Full tym emp

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWagesProblemWithuseObj
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            CheckEmployeeFullTime obj = new CheckEmployeeFullTime();
+            obj.EmployeeAttendence();
+        }
+    }
+}
