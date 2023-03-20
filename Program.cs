2fad3f7ee6f78263d34e03ebe9054a921a60797f
Aakankshap16 <aakankshap971@gmail.com>
2023-03-20 15:24:31 +0530

    call program for uc8

--- Program.cs
+++ Program.cs
@@ -4,9 +4,18 @@
     {
         public static void Main(string[] args) 
         {
-            // refactor the code to write aclass method to compute emp wages
+            // Compute Employee wages for multiple Company
+
+            Console.WriteLine("Enter the name of the company Dmart or Relince:");
+            string company = Console.ReadLine();
+
             EmployeeWageCode obj = new EmployeeWageCode();
-            obj.ComputeEmpWage();
+            if (company == "Dmart")
+            {
+                obj.ComputeEmpWage(company, 20, 10, 90);
+            }
+            else if (company == "Relince") { obj.ComputeEmpWage(company, 20, 10, 90); }
+            else { Console.WriteLine("Please enter correct company!!"); }
         }
     }
 }
